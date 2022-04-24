6dbb8506ff368da46618c6120e12e23bdac9d3c4
MayoresMycaMae <mayoresmyca@gmail.com>
2022-04-25 03:01:15 +0800

    images changed

--- account.php
+++ account.php
@@ -1,3 +1,16 @@
+<?php
+session_start();
+
+if (isset($_SESSION['ses_username'])===false){
+    header("Location: index.php?logfirst");
+} else if (isset($_REQUEST['logout'])===true){
+    session_destroy();
+    header("Location: index.php?logout");
+}
+
+?>
+
+
 <!doctype html>
                         <html>
                             <head>
@@ -13,32 +26,34 @@
     transform: translateY(5rem)
 }
 .cover {
-    background-image: url('images/photo-profile-background.avif');
+    background-image: url('images/bg.jpg');
     background-size: cover;
-    background-repeat: no-repeat
+    background-repeat: no-repeat;
+
 }
 
 body {
-    background: #654ea3;
-    background: linear-gradient(to right, #e96443, #904e95);
+    background: #7b80b5;
     min-height: 100vh;
     overflow-x: hidden
 }</style>
                                 </head>
                                 <body oncontextmenu='return false' class='snippet-body'>
-                                <div class="row py-5 px-4">
+                                <div class="row py-10 px-10">
     <div class="col-md-5 mx-auto">
         <!-- Profile widget -->
         <div class="bg-white shadow rounded overflow-hidden">
-            <div class="px-4 pt-0 pb-4 cover">
+            <div class="px-4 pt-3 pb-0 cover">
                 <div class="media align-items-end profile-head">
-                    <div class="profile mr-3"><img src="images/photo-profile.avif" alt="..." width="150" class="rounded mb-2 img-thumbnail"><a href="#" class="btn btn-outline-dark btn-sm btn-block">Edit Profile</a>
+                    <div class="profile mr-5"><img src="images/my.png" alt="..." width="150" >
+                        <a href="?logout" class="btn btn-outline-dark btn-sm btn-block mt-1 ">Sign Out</a>
 
 
                     </div>
                     <div class="media-body mb-5 text-white">
-                        <h4 class="mt-0 mb-0">Ryan Clifford L. Perez</h4>
-                        <p class="small mb-4"> <i class="fas fa-map-marker-alt mr-2"></i>Marinduque PH
+                        <h4 class="mt-5 mb-4"> <?php echo $_SESSION['ses_fullname']; ?> </h4>
+                        <p class="small mb-5"> <i class="fas fa-map-marker-alt mr-1"></i>
+                            <?php echo $_SESSION['ses_address']; ?>
                          </p>
                     </div>
                 </div>
@@ -46,13 +61,13 @@ body {
             <div class="bg-light p-4 d-flex justify-content-end text-center">
                 <ul class="list-inline mb-0">
                     <li class="list-inline-item">
-                        <h5 class="font-weight-bold mb-0 d-block">21,150</h5><small class="text-muted"> <i class="fas fa-image mr-1"></i>Photos</small>
+                        <h5 class="font-weight-bold mb-0 d-block">1,500</h5><small class="text-muted"> <i class="fas fa-image mr-1"></i>Photos</small>
                     </li>
                     <li class="list-inline-item">
-                        <h5 class="font-weight-bold mb-0 d-block">756,450</h5><small class="text-muted"> <i class="fas fa-user mr-1"></i>Followers</small>
+                        <h5 class="font-weight-bold mb-0 d-block">1M</h5><small class="text-muted"> <i class="fas fa-user mr-1"></i>Followers</small>
                     </li>
                     <li class="list-inline-item">
-                        <h5 class="font-weight-bold mb-0 d-block">340</h5><small class="text-muted"> <i class="fas fa-user mr-1"></i>Following</small>
+                        <h5 class="font-weight-bold mb-0 d-block">250</h5><small class="text-muted"> <i class="fas fa-user mr-1"></i>Following</small>
                     </li>
                 </ul>
             </div>
@@ -60,7 +75,7 @@ body {
             <div class="px-4 py-3">
                 <h5 class="mb-0">Expertise </h5>
                 <div class="p-4 rounded shadow-sm bg-light">
-                    <p class="font-italic mb-0">Full-stack Web Developer</p>
+                    <p class="font-italic mb-0">Web Developer</p>
                     <p class="font-italic mb-0">Mobile App Developer</p>
                     <p class="font-italic mb-0">Photographer/Videographer</p>
                 </div>
@@ -72,10 +87,11 @@ body {
                 </div>
                 <div class="row">
                     
-                    <div class="col-lg-6 mb-2 pr-lg-1"><img src="images/photo-1.avif" alt="" class="img-fluid rounded shadow-sm"></div>              
-                    <div class="col-lg-6 mb-2 pl-lg-1"><img src="images/photo-2.avif" alt="" class="img-fluid rounded shadow-sm"></div>                
-                    <div class="col-lg-6 pr-lg-1 mb-2"><img src="images/photo-3.avif" alt="" class="img-fluid rounded shadow-sm"></div>
-                    <div class="col-lg-6 pl-lg-1"><img src="images/photo-4.avif" alt="" class="img-fluid rounded shadow-sm"></div>
+                    <div class="col-lg-4 mb-2 pr-lg-1 p-2"><img src="images/one.jpg" alt="" class="img-fluid rounded shadow-sm"></div>              
+                    <div class="col-lg-4 mb-2 pr-lg-1 p-2"><img src="images/fpink.jpg" alt="" class="img-fluid rounded shadow-sm"></div>  
+                    <div class="col-lg-4 mb-2 pr-lg-1 p-2"><img src="images/two.jpg" alt="" class="img-fluid rounded shadow-sm"></div>               
+                    <div class="col-lg-6 pr-lg-1 mb-1 p-2"><img src="images/cousin.jpg" alt="" class="img-fluid rounded shadow-sm"></div>
+                    <div class="col-lg-6 pr-lg-1 p-2"><img src="images/cards.jpg" alt="" class="img-fluid rounded shadow-sm"></div>
                 </div> 
             </div>
 
